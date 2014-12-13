2af02c1e0c46ca4ab76b819515c08a1367d4fa29
james taylor <taylorjames9@james-taylors-MacBook-Pro.local>
2014-12-12 18:12:33 -0600

    working on getting swappable items between characters

--- SyrProject/Assets/Scripts_2/Drag_Item.cs
+++ SyrProject/Assets/Scripts_2/Drag_Item.cs
@@ -11,6 +11,7 @@ public class Drag_Item : MonoBehaviour
 	private Vector3 offset;
 	private Vector3 startPosition;
 	private bool touchingWhereIWantToBe;
+	private Vector2 itemGoesToThisSpot;
 
 	void Start(){
 		startPosition = transform.position;
@@ -19,19 +20,39 @@ public class Drag_Item : MonoBehaviour
 
 	}
 
-	void OnCollisionStay2D(Collision2D coll) {
-		switch(coll.gameObject.name){
-		case "Orange_Spot1":
-			QueueScript1 orangeQueueScript = this.GetComponentInParent<QueueScript1>(); 
-			orangeQueueScript.myItemObjects.Insert(0, (Item_Set)coll.gameObject.GetComponent<Item_Set>());
-			orangeQueueScript.displayNewQueueVisualFromOwnerQueueList();
-			touchingWhereIWantToBe = true;
+	void OnTriggerStay2D(Collider2D coll) {
+		Debug.Log("COLLIDER ON SYR GOT COLLIDED");
+		Debug.Log("COLLIDING WITH : "+ coll.gameObject.name);
+		//if(coll.gameObject is Q_Spot_Coll_Script){
+			if(coll.gameObject.name == "Orange_Spot1"){
+				Debug.Log("TOUCHING ORANGE SPOT 1");
+				QueueScript1 orangeQueueScript = this.GetComponentInParent<QueueScript1>(); 
+				orangeQueueScript.myItemObjects.Insert(0, (Item_Set)coll.gameObject.GetComponent<Item_Set>());
+				orangeQueueScript.displayNewQueueVisualFromOwnerQueueList();
+				touchingWhereIWantToBe = true;
+				//transform.position = coll.transform.position;
+				itemGoesToThisSpot = coll.transform.position;
+				
+				/**
+				 * Make the parent of this thing the parent of the other object that you collided with
+				 * */
 
-			break;
-		default:
-			break;
-		}
+			}
 
+
+
+//			switch(coll.gameObject.name){
+//				case "Orange_Spot1":
+//					Debug.Log("TOUCHING ORANGE SPOT 1");
+//					QueueScript1 orangeQueueScript = this.GetComponentInParent<QueueScript1>(); 
+//					orangeQueueScript.myItemObjects.Insert(0, (Item_Set)coll.gameObject.GetComponent<Item_Set>());
+//					orangeQueueScript.displayNewQueueVisualFromOwnerQueueList();
+//					touchingWhereIWantToBe = true;
+//					transform.position = coll.transform.position;
+//					break;
+//			default:
+//				break;
+			//}
 	}
 	
 	void OnMouseDown()
@@ -44,16 +65,13 @@ public class Drag_Item : MonoBehaviour
 	void OnMouseDrag()
 	{
 		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-		
 		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
 		transform.position = curPosition;
-		
 	}
 
 	void OnMouseUp(){
 		if(touchingWhereIWantToBe){
-
-
+			transform.position = itemGoesToThisSpot;
 		}
 		else
 			transform.position = startPosition;
