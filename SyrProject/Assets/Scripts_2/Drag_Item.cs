a412033748727335ddcf735a59461ee3c17720fc
james taylor <taylorjames9@james-taylors-MacBook-Pro.local>
2014-12-13 11:09:50 -0600

    Dragged item now gets inserted in new player queue.

--- SyrProject/Assets/Scripts_2/Drag_Item.cs
+++ SyrProject/Assets/Scripts_2/Drag_Item.cs
@@ -12,6 +12,7 @@ public class Drag_Item : MonoBehaviour
 	private Vector3 startPosition;
 	private bool touchingWhereIWantToBe;
 	private Vector2 itemGoesToThisSpot;
+	private bool iAmBeingMoved;
 
 	void Start(){
 		startPosition = transform.position;
@@ -20,39 +21,26 @@ public class Drag_Item : MonoBehaviour
 
 	}
 
-	void OnTriggerStay2D(Collider2D coll) {
+	void OnTriggerEnter2D(Collider2D coll) {
 		Debug.Log("COLLIDER ON SYR GOT COLLIDED");
 		Debug.Log("COLLIDING WITH : "+ coll.gameObject.name);
 		//if(coll.gameObject is Q_Spot_Coll_Script){
+		if(iAmBeingMoved){
 			if(coll.gameObject.name == "Orange_Spot1"){
 				Debug.Log("TOUCHING ORANGE SPOT 1");
-				QueueScript1 orangeQueueScript = this.GetComponentInParent<QueueScript1>(); 
-				orangeQueueScript.myItemObjects.Insert(0, (Item_Set)coll.gameObject.GetComponent<Item_Set>());
+				QueueScript1 orangeQueueScript = coll.GetComponentInParent<QueueScript1>(); 
+				orangeQueueScript.myItemObjects.Insert(0, this.GetComponent<Item_Set>());
+				
 				orangeQueueScript.displayNewQueueVisualFromOwnerQueueList();
 				touchingWhereIWantToBe = true;
 				//transform.position = coll.transform.position;
-				itemGoesToThisSpot = coll.transform.position;
+				itemGoesToThisSpot = (Vector2) coll.transform.position;
 				
 				/**
 				 * Make the parent of this thing the parent of the other object that you collided with
 				 * */
-
 			}
-
-
-
-//			switch(coll.gameObject.name){
-//				case "Orange_Spot1":
-//					Debug.Log("TOUCHING ORANGE SPOT 1");
-//					QueueScript1 orangeQueueScript = this.GetComponentInParent<QueueScript1>(); 
-//					orangeQueueScript.myItemObjects.Insert(0, (Item_Set)coll.gameObject.GetComponent<Item_Set>());
-//					orangeQueueScript.displayNewQueueVisualFromOwnerQueueList();
-//					touchingWhereIWantToBe = true;
-//					transform.position = coll.transform.position;
-//					break;
-//			default:
-//				break;
-			//}
+		}
 	}
 	
 	void OnMouseDown()
@@ -67,9 +55,11 @@ public class Drag_Item : MonoBehaviour
 		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
 		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
 		transform.position = curPosition;
+		iAmBeingMoved = true;
 	}
 
 	void OnMouseUp(){
+		iAmBeingMoved = false;
 		if(touchingWhereIWantToBe){
 			transform.position = itemGoesToThisSpot;
 		}
