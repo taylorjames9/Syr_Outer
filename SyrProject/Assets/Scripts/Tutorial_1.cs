2acef2629faba2639594308ef64d47e4c48d8696
james taylor <taylorjames9@james-taylors-MacBook-Pro.local>
2014-12-18 20:06:50 -0600

    More tutorial progress

--- SyrProject/Assets/Scripts/Tutorial_1.cs
+++ SyrProject/Assets/Scripts/Tutorial_1.cs
@@ -12,54 +12,56 @@ public class Tutorial_1 : MonoBehaviour
 
 		void Update ()
 		{
+		Debug.Log ("tutorial State is: "+nTutState);
 				switch (nTutState) {
+				//This is the main
 				case 0:
-						tutorialPanels [nTutState].gameObject.SetActive (true);
-						for (int i=0; i< tutorialPanels.Count; i++) {
-								if (i != nTutState) {
-										tutorialPanels [i].gameObject.SetActive (false);
-								}
-						}
+						openRelevantTutCloseOthers();
 						break;
 				case 1:
-						tutorialPanels [nTutState].gameObject.SetActive (true);
-						for (int i=0; i< tutorialPanels.Count; i++) {
-							if (i != nTutState) {
-								tutorialPanels [i].gameObject.SetActive (false);
-							}
-						}
+				//This is the non-main
+						openRelevantTutCloseOthers();
 						break;
 				case 2:
-						tutorialPanels [nTutState].gameObject.SetActive (true);
-						for (int i=0; i< tutorialPanels.Count; i++) {
-							if (i != nTutState) {
-								tutorialPanels [i].gameObject.SetActive (false);
-							}
-						}
+				//Tap to see items
+						openRelevantTutCloseOthers();
 						if(mainPlayerScript.getTapOnMeCounter() == 1){
 							increaseTutStateBy1(1);
 						}
 			
 						break;
 				case 3:
-						tutorialPanels [nTutState].gameObject.SetActive (true);
-						for (int i=0; i< tutorialPanels.Count; i++) {
-							if (i != nTutState) {
-								tutorialPanels [i].gameObject.SetActive (false);
-							}
-						}
+				//Try Moving
+						openRelevantTutCloseOthers();
 						if(Drag_Item.tapOnSyringeCounter > 0){
 							increaseTutStateBy1(1);
 						}
 
 						break;
 				case 4:
-					tutorialPanels [nTutState].gameObject.SetActive (true);
-					for (int i=0; i< tutorialPanels.Count; i++) {
-						if (i != nTutState) {
-							tutorialPanels [i].gameObject.SetActive (false);
-						}
+				//Syringe Order
+						openRelevantTutCloseOthers();
+					break;
+				case 5:
+				//Color Syringes do this...black syringes do that. 
+						openRelevantTutCloseOthers();
+					break;
+				case 6:
+				//See Arrow
+					openRelevantTutCloseOthers();
+					if(mainPlayerScript.getTapOnMeCounter() > 1){
+						increaseTutStateBy1(1);
 					}
+
+					break;
+				case 7:
+					openRelevantTutCloseOthers();
+					break;
+				case 8:
+					openRelevantTutCloseOthers();
+					break;
+				case 9:
+					openRelevantTutCloseOthers();
 					break;
 
 				default:
@@ -70,6 +72,16 @@ public class Tutorial_1 : MonoBehaviour
 
 	public void increaseTutStateBy1(int thisNum){
 		nTutState += thisNum;
+	}
+
+	private void openRelevantTutCloseOthers(){
+		tutorialPanels [nTutState].gameObject.SetActive (true);
+		for (int i=0; i< tutorialPanels.Count; i++) {
+			if (i != nTutState) {
+				tutorialPanels [i].gameObject.SetActive (false);
+			}
+		}
+
 	}
 
 
