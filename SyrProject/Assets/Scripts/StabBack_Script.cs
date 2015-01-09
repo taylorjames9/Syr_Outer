7e7822d4c524c32869003384a44c138439cbfa12
taylorjames9 <taylorjames9@gmail.com>
2015-01-09 16:13:10 -0600

    feeling much closer to the stab back

--- SyrProject/Assets/Scripts/StabBack_Script.cs
+++ SyrProject/Assets/Scripts/StabBack_Script.cs
@@ -7,19 +7,30 @@ public class StabBack_Script : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		this.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,0.15f);
+		//this.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,0.15f);
+		toggleStabBack(0.15f);
 	}
 
 	void OnMouseUp(){
 		tapsOnSwitchBack++;
 		if(tapsOnSwitchBack % 2 == 1){
-		this.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,1f);
-		this.transform.GetComponentInParent<Character>().setStabBack(true);
-		Debug.Log("Setting stab back");
+			toggleStabBack(1.0f);
+			Debug.Log("Setting stab back");
 		}
 		else {
-			this.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,0.15f);
+			toggleStabBack(0.15f);
+		}
+	}
+
+	public string toggleStabBack(float alph){
+		this.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f, alph);
+		if(alph< 0.5f){
 			this.transform.GetComponentInParent<Character>().setStabBack(false);
+			return "grey";
+		}
+		else{
+			this.transform.GetComponentInParent<Character>().setStabBack(true);
+			return "black";
 		}
 	}
 
